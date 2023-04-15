3c870cd0099a8a58a1c1835d00398dd10a5b0026
Alaoefyn <121045925+Alaoefyn@users.noreply.github.com>
2023-04-15 12:58:36 +0300

    Create HexBnryCdrDcr.py

---
+++ HexBnryCdrDcr.py
@@ -0,0 +1,36 @@
+def hex_to_string(hex_input):
+    return bytes.fromhex(hex_input).decode('utf-8')
+
+def binary_to_string(binary_input):
+    return ''.join(chr(int(binary_input[i:i+8], 2)) for i in range(0, len(binary_input), 8))
+
+def string_to_hex(string_input):
+    return ''.join(hex(ord(c))[2:] for c in string_input)
+
+def string_to_binary(string_input):
+    return ''.join(format(ord(c), '08b') for c in string_input)
+
+def main():
+    while True:
+        user_input = input("Enter 'h' for Hexadecimal, 'b' for Binary and 's' for String Input: ")
+        if user_input.lower() == 'h':
+            hex_input = input("Enter Hexadecimal Input: ")
+            print("Decoded String: ", hex_to_string(hex_input))
+        elif user_input.lower() == 'b':
+            binary_input = input("Enter Binary Input: ")
+            print("Decoded String: ", binary_to_string(binary_input))
+        elif user_input.lower() == 's':
+            string_input = input("Enter String Input: ")
+            sub_input = input("Enter 'h' for Hexadecimal, 'b' for Binary Output: ")
+            if sub_input.lower() == 'h':
+                print("Hexadecimal Output: ", string_to_hex(string_input))
+            elif sub_input.lower() == 'b':
+                print("Binary Output: ", string_to_binary(string_input))
+        else:
+            print("Invalid Input!")
+        cont = input("Do you want to continue? (Y/N): ")
+        if cont.lower() == 'n':
+            break
+
+if __name__ == '__main__':
+    main()
